eac0279a05934c37fe48d474951ad3c70920b32b
AlexGit2012 <a.karavaychik@gmail.com>
2020-09-07 02:45:12 +0300

    fixed project

---
+++ precache-manifest.443c1b0a01e5ea92852a42a70afb591f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8498b2665076a31a3c099c6b7cd011c8",
+    "url": "/unl_test_dood_added/index.html"
+  },
+  {
+    "revision": "10fc5f3831453cd9e1e6",
+    "url": "/unl_test_dood_added/static/css/main.f3f53857.chunk.css"
+  },
+  {
+    "revision": "d7736a0cb68b204005c2",
+    "url": "/unl_test_dood_added/static/js/2.177a44cc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/unl_test_dood_added/static/js/2.177a44cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fc5f3831453cd9e1e6",
+    "url": "/unl_test_dood_added/static/js/main.a370e7ec.chunk.js"
+  },
+  {
+    "revision": "3c8a53f929655418f81a",
+    "url": "/unl_test_dood_added/static/js/runtime-main.bf470547.js"
+  },
+  {
+    "revision": "25593cb9f5e4107ffbfa23369bc55216",
+    "url": "/unl_test_dood_added/static/media/Illustration.25593cb9.png"
+  },
+  {
+    "revision": "27400a48740aeb0a78b71a9d08527f1a",
+    "url": "/unl_test_dood_added/static/media/menuOnHover.27400a48.svg"
+  }
+]);
